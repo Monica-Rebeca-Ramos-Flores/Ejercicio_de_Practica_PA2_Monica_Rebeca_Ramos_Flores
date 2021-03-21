17c9ac000e11e1383fd5b437fd75a077830a92e3
Monica Rebeca Ramos Flores <monicarebecarf2000@gmail.com>
2021-03-20 22:38:26 -0600

    Version 1 Ejercicio de Practica PA2 Monica Rebeca Ramos Flores

---
+++ src/main/java/com/example/demo/modelos/Persona.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.modelos;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author user
+ */
+@Data
+@NoArgsConstructor
+@Entity
+public class Persona {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String monica;
+    private String rebeca;
+    private String ramos;
+    private String flores;
+}
